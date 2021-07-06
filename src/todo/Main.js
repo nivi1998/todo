83ca924f9ba3034307ab9ce0a3d66138eecbc090
nivi1998 <niveditasinghrajput801@gmail.com>
2021-07-06 13:43:34 +0530

    hello chnge

--- src/todo/Main.js
+++ src/todo/Main.js
@@ -1,12 +1,1 @@
-import React from 'react'
 
-export default function Main() {
-    return (
-        <>
-        <h1 className="head">To Do List</h1>
-        <input className="input" />
-        <button type="submit">Submit</button>
-            
-        </>
-    )
-}
